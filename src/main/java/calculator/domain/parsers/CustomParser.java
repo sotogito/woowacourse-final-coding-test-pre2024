23924163b84c0a1876427541c3f9ecac88e3b9e2
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-21 15:38:18 +0900

    feat : 커스텀 구분자 파싱 로직 구현

---
+++ src/main/java/calculator/domain/parsers/CustomParser.java
@@ -0,0 +1,58 @@
+package calculator.domain.parsers;
+
+import calculator.domain.CalculateValue;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomParser implements DelimiterParser {
+    private final static String CUSTOM_PATTERN = "//(.)\\n";
+
+    public static void main(String[] args) {
+        DelimiterParser customParser = new CustomParser();
+        customParser.parse("//1\n//2\n1,2,3",new CalculateValue());
+    }
+
+    @Override
+    public void parse(String input, CalculateValue calculateValue) {
+        Pattern pattern = Pattern.compile(CUSTOM_PATTERN);
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> customDelimiters = new ArrayList<>();
+
+        while (matcher.find()) {
+            customDelimiters.add(matcher.group(1));
+        }
+
+        String delimiterSplit = String.join("|", customDelimiters);
+        String value = input.replaceAll(CUSTOM_PATTERN, "");
+
+        String[] splitInput = value.split(delimiterSplit);
+
+        for(String s : splitInput) {
+            s = s.trim();
+
+
+            if(s.isEmpty()){
+                calculateValue.addNumber(0);
+                continue;
+            }
+
+            int number = 0;
+            try {
+                number = Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("기본 구분자 외에 다른 구분자가 포함되었습니다.");
+            }
+
+            if(number < 0){
+                throw new IllegalArgumentException("계산값은 양수만 가능합니다.");
+            }
+
+            calculateValue.addNumber(number);
+        }
+
+
+    }
+}
