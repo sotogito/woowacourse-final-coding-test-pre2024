c4c1193ce015a3e707d881511363abea50c6bbe9
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-21 16:09:57 +0900

    feat : 입력 문자열에 따라 parser 반환하기

---
+++ src/main/java/calculator/domain/InputValueMatcher.java
@@ -0,0 +1,29 @@
+package calculator.domain;
+
+import calculator.domain.parsers.BasicParser;
+import calculator.domain.parsers.CustomParser;
+import calculator.domain.parsers.DelimiterParser;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValueMatcher {
+
+    private final DelimiterParser baseDelimiterParser;
+    private final DelimiterParser customDelimiterParser;
+
+    public InputValueMatcher() {
+        baseDelimiterParser = new BasicParser();
+        customDelimiterParser = new CustomParser();
+    }
+
+    public DelimiterParser findDelimiterParser(String input) {
+        Matcher customDelimiterMatcher = Pattern.compile("//(.)\\n").matcher(input);
+        if (customDelimiterMatcher.find()) {
+            return customDelimiterParser;
+        }
+        return baseDelimiterParser;
+    }
+
+}
