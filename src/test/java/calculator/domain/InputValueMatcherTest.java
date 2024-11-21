2c83e7fd5655a22cd18a4388442d527b024040cf
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-21 16:10:03 +0900

    test : 입력 문자열에 따라 parser 반환하기 테스트

---
+++ src/test/java/calculator/domain/InputValueMatcherTest.java
@@ -0,0 +1,26 @@
+package calculator.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import calculator.ParserType;
+import calculator.domain.parsers.DelimiterParser;
+import org.junit.jupiter.api.Test;
+
+class InputValueMatcherTest {
+    private InputValueMatcher inputValueMatcher = new InputValueMatcher();
+
+    @Test
+    void 기본_구분자_파서_반환(){
+        assertEquals(
+                ParserType.BASIC,
+                inputValueMatcher.findDelimiterParser("1,2,3").getParserType());
+    }
+
+    @Test
+    void 커스텀_구분자_파서_반환(){
+        assertEquals(
+                ParserType.CUSTOM,
+                inputValueMatcher.findDelimiterParser("//$\n1$2$3").getParserType());
+    }
+
+}
