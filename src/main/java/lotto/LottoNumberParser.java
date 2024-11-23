d7c6902d72da82ae8d62b337a1577c3735d62381
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-23 15:25:53 +0900

    feat : 입력값 정수 리스트 변경 Parser 작성

---
+++ src/main/java/lotto/LottoNumberParser.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LottoNumberParser {
+    /**
+     * 콤마 외 다른 문자 Input : 숫자
+     */
+
+    public List<Integer> parse(String input) {
+        List<Integer> result = new ArrayList<>();
+
+        validateNameSpecialChar(input);
+        String[] splitValue = input.split(",");
+
+        try {
+            for (String s : splitValue) {
+                s = s.trim();
+                result.add(Integer.parseInt(s));
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("로또는 숫자로 입력해주세요.");
+        }
+
+        return result;
+    }
+
+    private void validateNameSpecialChar(String name) {
+        Matcher matcher = Pattern.compile("[!@#$%^&*().?\":{}|<>]").matcher(name);
+        if (matcher.find()) {
+            throw new IllegalArgumentException("로또 번호는 콤마(,)로 구분해주세요.");
+        }
+    }
+}
