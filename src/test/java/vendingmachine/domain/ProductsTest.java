d14cdf29d411d674eaa7eb0b19c7cdc48a2d1a0b
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-25 17:31:35 +0900

    feat : 자판기 보유 상품 최소 금액 상품 반환 테스트

---
+++ src/test/java/vendingmachine/domain/ProductsTest.java
@@ -0,0 +1,40 @@
+package vendingmachine.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ProductsTest {
+    private Products products = new Products();
+
+    @Test
+    void 최소_가격_상품_반환_확인() {
+        List<Product> productList = new ArrayList<>();
+        Product expected = new Product("콜라", 100, 5);
+
+        productList.add(expected);
+        productList.add(new Product("사이다", 1000, 2));
+
+        products.addAllProducts(productList);
+        Product actual = products.minimumPriceProduct();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void 최소_가격_sold_out시_그_다음_최소값_상품_반환_확인() {
+        List<Product> productList = new ArrayList<>();
+        Product expected = new Product("사이다", 1000, 2);
+
+        productList.add(new Product("콜라", 100, 0));
+        productList.add(expected);
+
+        products.addAllProducts(productList);
+        Product actual = products.minimumPriceProduct();
+
+        assertEquals(expected, actual);
+    }
+
+}
