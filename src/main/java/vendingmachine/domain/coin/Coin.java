603681a954c333db3d3ad1287b4217e121b983b5
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-26 19:55:35 +0900

    refactor : Coin 동전 리스트 반환시 내림차순으로

--- src/main/java/vendingmachine/domain/coin/Coin.java
+++ src/main/java/vendingmachine/domain/coin/Coin.java
@@ -1,6 +1,7 @@
 package vendingmachine.domain.coin;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.List;
 
@@ -8,7 +9,8 @@ public enum Coin {
     COIN_500(500),
     COIN_100(100),
     COIN_50(50),
-    COIN_10(10);
+    COIN_10(10),
+    ;
 
     private final int amount;
 
@@ -28,11 +30,13 @@ public enum Coin {
         return map;
     }
 
+    //note 내림차순이 되도록
     public static List<Integer> getAmountList() {
         List<Integer> result = new ArrayList<>();
         for (Coin coin : Coin.values()) {
             result.add(coin.getAmount());
         }
+        result.sort(Comparator.reverseOrder());
         return result;
     }
 
@@ -45,5 +49,12 @@ public enum Coin {
         return null;
     }
 
-    // 추가 기능 구현
+    /**
+     * 객체를 비교ㅕ하는거랑 단순히 데이터를 바교하는거랑 달라 get을 해올수있으면 comparator 내부에서 아예 지정해주기 위해서는 comporable
+     * <p>
+     * Enum은??
+     */
+    public static final Comparator<Coin> BY_VALUE_ASC = Comparator.comparingInt(Coin::getAmount);
+    public static final Comparator<Coin> BY_VALUE_DESC = Comparator.comparingInt(Coin::getAmount).reversed();
+
 }
