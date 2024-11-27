01a582b5de3c8e7eef11294e6f99914cf4d1ae9d
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-27 14:54:17 +0900

    feat : 시도 횟수 객체 생성

---
+++ src/main/java/bridge/domain/AttemptManager.java
@@ -0,0 +1,39 @@
+package bridge.domain;
+
+public class AttemptManager {
+    private int totalAttempts;
+    private int order;
+
+    public AttemptManager() {
+        this.totalAttempts = 0;
+        this.order = 0;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public void updateTotalAttempts() {
+        this.totalAttempts += 1;
+    }
+
+    public void updateOrder() {
+        this.order += 1;
+    }
+
+    public void clearOrder() {
+        this.order = 0;
+    }
+
+    public int getOrder() {
+        return this.order;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("총 시도한 횟수: %d", totalAttempts + 1);
+    }
+
+
+}
