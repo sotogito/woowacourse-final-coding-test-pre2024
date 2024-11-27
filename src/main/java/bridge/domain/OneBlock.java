eb508237384b3ec68469962159f236ab8bf4ac9f
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-27 14:06:48 +0900

    feat : 다리1칸 객체 생성

---
+++ src/main/java/bridge/domain/OneBlock.java
@@ -0,0 +1,13 @@
+package bridge.domain;
+
+public class OneBlock {
+    private final String location;
+    private boolean isPass;
+
+    public OneBlock(String location) {
+        this.location = location;
+        this.isPass = false;
+    }
+
+
+}
