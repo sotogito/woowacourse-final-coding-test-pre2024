c95729edf2e6f2a7cb10f09d6d831583de3356a9
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-30 02:14:40 +0900

    fix : 강남역 오타수정

--- src/main/java/subway/controller/function/LineController.java
+++ src/main/java/subway/controller/function/LineController.java
@@ -1,7 +1,11 @@
 package subway.controller.function;
 
+import subway.constants.LineFunction;
 import subway.constants.MainFunction;
 import subway.service.LineService;
+import subway.view.Input;
+import subway.view.LineInput;
+import subway.view.Output;
 
 public class LineController {
     private final LineService lineService;
@@ -12,11 +16,66 @@ public class LineController {
 
     public void run() {
         while (true) {
-            /**
-             * 만약 B 누르면 break
-             */
+            Output.printLineFunctionList();
+            LineFunction function = inputLineFunction();
+            if (function.equals(LineFunction.BACK)) {
+                break;
+            }
+            if (function.equals(LineFunction.ADD_LINE)) {
+                add();
+            } else if (function.equals(LineFunction.DELETE_LINE)) {
+                delete();
+            } else if (function.equals(LineFunction.PRINT_LINE)) {
+                print();
+            }
+
         }
     }
+
+    private void add() {
+        while (true) {
+            try {
+                String line = LineInput.inputAddLineName();
+                String upStation = LineInput.inputUpStationName();
+                String downStation = LineInput.inputDownStationName();
+
+                lineService.addLine(line, upStation, downStation);
+                return;
+            } catch (IllegalArgumentException e) {
+                Output.printError(e.getMessage());
+            }
+        }
+    }
+
+
+    private void delete() {
+        while (true) {
+            try {
+                String line = LineInput.inputDeleteLineName();
+
+                lineService.deleteLine(line);
+                return;
+            } catch (IllegalArgumentException e) {
+                Output.printError(e.getMessage());
+            }
+        }
+    }
+
+    private void print() {
+        Output.printLines(lineService.getAllLines());
+    }
+
+
+    private LineFunction inputLineFunction() {
+        while (true) {
+            try {
+                return LineFunction.findByValue(Input.inputFunction());
+            } catch (IllegalArgumentException e) {
+                Output.printError(e.getMessage());
+            }
+        }
+    }
+
 
     public boolean isFunctionType(MainFunction inputFunctionType) {
         return MainFunction.LINE_MANAGEMENT.equals(inputFunctionType);
