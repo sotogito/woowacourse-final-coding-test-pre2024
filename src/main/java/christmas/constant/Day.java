291b93c717b485c3b6f81eced411e9e9f06e6df2
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-03 18:29:16 +0900

    feat : 요일 Enum으로 주말 재정의

---
+++ src/main/java/christmas/constant/Day.java
@@ -0,0 +1,35 @@
+package christmas.constant;
+
+import java.time.DayOfWeek;
+
+public enum Day {
+    SUN(DayOfWeek.SUNDAY, false),
+    MON(DayOfWeek.MONDAY, false),
+    TUE(DayOfWeek.TUESDAY, false),
+    WEN(DayOfWeek.WEDNESDAY, false),
+    THU(DayOfWeek.THURSDAY, false),
+    FRI(DayOfWeek.FRIDAY, true),
+    SAT(DayOfWeek.SATURDAY, true);
+
+    private final DayOfWeek mDayOfWeek;
+    private final boolean isWeekend;
+
+    Day(DayOfWeek dayOfWeek, boolean isWeekend) {
+        mDayOfWeek = dayOfWeek;
+        this.isWeekend = isWeekend;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public static Day find(DayOfWeek dayOfWeek) {
+        for (Day day : Day.values()) {
+            if (day.mDayOfWeek == dayOfWeek) {
+                return day;
+            }
+        }
+        return null;
+    }
+
+}
