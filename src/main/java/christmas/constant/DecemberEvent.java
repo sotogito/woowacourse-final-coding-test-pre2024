f082af50fca96ffeb7e1020b83352d9fb161e91e
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-03 18:29:48 +0900

    feat : 12월 이벤트 Enum 작성

---
+++ src/main/java/christmas/constant/DecemberEvent.java
@@ -0,0 +1,20 @@
+package christmas.constant;
+
+public enum DecemberEvent {
+    CHRISTMAS_D_DAY_DISCOUNT("크리스마스 디데이 할인"),
+    WEEKDAY_DISCOUNT("평일 할인"),
+    WEEKEND_DISCOUNT("주말 할인"),
+    SPECIAL_DISCOUNT("특별 할인"),
+    GIFT_EVENT("증정 이벤트"),
+    BADGE_EVENT("배지 이벤트");
+
+    private final String name;
+
+    DecemberEvent(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
