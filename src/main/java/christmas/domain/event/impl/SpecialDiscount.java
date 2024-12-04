213694c0922b2a355c3a69c03e31cec5e52f4806
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-04 16:30:53 +0900

    feat : 스페셜 할인 클래스 생성

---
+++ src/main/java/christmas/domain/event/impl/SpecialDiscount.java
@@ -0,0 +1,35 @@
+package christmas.domain.event.impl;
+
+import christmas.constant.DecemberEvent;
+import christmas.domain.EventPlan;
+import christmas.domain.event.Discount;
+import christmas.domain.user.Cart;
+import christmas.domain.user.Schedule;
+import christmas.domain.user.Wallet;
+import java.time.LocalDate;
+import java.util.List;
+
+public class SpecialDiscount implements Discount {
+    private final static LocalDate start = LocalDate.of(2023, 12, 1);
+    private final static LocalDate end = LocalDate.of(2023, 12, 31);
+    private final static List<Integer> SPECIAL_DAYS = List.of(3, 10, 17, 24, 31, 25);
+
+    private final static int BASE_DISCOUNT_PRICE = 1000;
+
+
+    @Override
+    public boolean isValidDate(Schedule schedule) {
+        return schedule.isValidDate(start, end) && schedule.isValidSpecialDate(SPECIAL_DAYS);
+    }
+
+    @Override
+    public void apply(Schedule schedule, Cart cart, Wallet wallet, EventPlan eventPlan) {
+        eventPlan.addAppliedEvent(this, BASE_DISCOUNT_PRICE);
+    }
+
+    @Override
+    public DecemberEvent getEventType() {
+        return DecemberEvent.SPECIAL_DISCOUNT;
+    }
+
+}
