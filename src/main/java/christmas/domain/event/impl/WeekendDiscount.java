c8f6ac95ee15e2f24765e302ff4a757cf6d46f15
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-04 16:30:41 +0900

    feat : 주말, 평일 할인 클래스 생성

---
+++ src/main/java/christmas/domain/event/impl/WeekendDiscount.java
@@ -0,0 +1,38 @@
+package christmas.domain.event.impl;
+
+import christmas.constant.Category;
+import christmas.constant.DecemberEvent;
+import christmas.domain.EventPlan;
+import christmas.domain.dto.CategoryQuantityDto;
+import christmas.domain.event.Discount;
+import christmas.domain.user.Cart;
+import christmas.domain.user.Schedule;
+import christmas.domain.user.Wallet;
+import java.time.LocalDate;
+
+public class WeekendDiscount implements Discount {
+    private final static LocalDate start = LocalDate.of(2023, 12, 1);
+    private final static LocalDate end = LocalDate.of(2023, 12, 31);
+
+    private final static int BASE_DISCOUNT_PRICE = 2023;
+    private final static Category APPLY_CATEGORY = Category.MAIN;
+
+
+    @Override
+    public boolean isValidDate(Schedule schedule) {
+        return schedule.isValidDate(start, end) && schedule.isWeekend();
+    }
+
+    @Override
+    public void apply(Schedule schedule, Cart cart, Wallet wallet, EventPlan eventPlan) {
+        CategoryQuantityDto dto = cart.findQuantityByCategory(APPLY_CATEGORY);
+        int totalDiscount = BASE_DISCOUNT_PRICE * dto.quantity();
+        eventPlan.addAppliedEvent(this, totalDiscount);
+    }
+
+    @Override
+    public DecemberEvent getEventType() {
+        return DecemberEvent.WEEKEND_DISCOUNT;
+    }
+
+}
