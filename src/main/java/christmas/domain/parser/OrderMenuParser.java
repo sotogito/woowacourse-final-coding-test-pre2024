443e930514ed4f8cbcfc9cf64d44ad10618e4c28
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-04 21:08:31 +0900

    feat : 주문 parser 후 DTO 반환 클래스 작성

---
+++ src/main/java/christmas/domain/parser/OrderMenuParser.java
@@ -0,0 +1,42 @@
+package christmas.domain.parser;
+
+import christmas.domain.dto.OrderItemDto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderMenuParser {
+
+    public static List<OrderItemDto> parse(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        List<OrderItemDto> result = new ArrayList<>();
+
+        String[] splitOrder = input.split(",");
+
+        for (String order : splitOrder) {
+            String[] menuAndQuantity = order.trim().split("-");
+
+            if (menuAndQuantity.length != 2) {
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            String menu = menuAndQuantity[0];
+            int quantity = changeNumber(menuAndQuantity[1]);
+
+            result.add(new OrderItemDto(menu, quantity));
+        }
+
+        return result;
+    }
+
+    private static int changeNumber(String quantity) {
+        try {
+            return Integer.parseInt(quantity);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+}
