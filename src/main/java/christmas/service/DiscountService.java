780f2a047527a358c82d74dccaf9726b3290695c
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-04 22:30:22 +0900

    feat : 할인, 배지, 증정품 적용 로직 분리

--- src/main/java/christmas/service/DiscountService.java
+++ src/main/java/christmas/service/DiscountService.java
@@ -6,6 +6,7 @@ import christmas.domain.event.Event;
 import christmas.domain.user.Cart;
 import christmas.domain.user.Schedule;
 import christmas.domain.user.Wallet;
+import java.util.Optional;
 
 /**
  * 이벤트 찾아서 플래너에 넣어주면 생성해줌
@@ -19,9 +20,27 @@ public class DiscountService {
 
     public EventPlan applyEvent(Schedule schedule, Cart cart, Wallet wallet) {
         EventPlan eventPlan = new EventPlan();
+
+        if (!wallet.canEvent()) {
+            return eventPlan;
+        }
+
         for (Event event : eventPlanner.getApplicableDateEvents(schedule)) {
             event.apply(schedule, cart, wallet, eventPlan);
         }
+
+        Optional<Event> giftEvent = eventPlanner.getGiftEvent(schedule);
+        if (giftEvent.isPresent()) {
+            Event gift = giftEvent.get();
+            gift.apply(schedule, cart, wallet, eventPlan);
+        }
+
+        Optional<Event> badgeGiftEvent = eventPlanner.getBadgeGiftEvent(schedule);
+        if (badgeGiftEvent.isPresent()) {
+            Event badge = badgeGiftEvent.get();
+            badge.apply(schedule, cart, wallet, eventPlan);
+        }
+
         return eventPlan;
     }
 
