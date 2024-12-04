2622ac388b47702b9ed074c11f374eafe78187e1
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-04 16:29:28 +0900

    feat : Event, 자식 Discount,Gift 인터페이스 생성

---
+++ src/main/java/christmas/domain/event/Event.java
@@ -0,0 +1,15 @@
+package christmas.domain.event;
+
+import christmas.constant.DecemberEvent;
+import christmas.domain.EventPlan;
+import christmas.domain.user.Cart;
+import christmas.domain.user.Schedule;
+import christmas.domain.user.Wallet;
+
+public interface Event {
+    boolean isValidDate(Schedule schedule);
+
+    void apply(Schedule schedule, Cart cart, Wallet wallet, EventPlan eventPlan);
+
+    DecemberEvent getEventType();
+}
