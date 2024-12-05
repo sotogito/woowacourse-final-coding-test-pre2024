34acd7b8a338f6870bd48e67737fd717c8eb15cf
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-05 15:55:44 +0900

    fix : 못먹는 음식 입력 시 존재하지 않는 메뉴의 경우 예외

--- src/main/java/menu/service/CoachHateMenuService.java
+++ src/main/java/menu/service/CoachHateMenuService.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import menu.domain.Coach;
 import menu.domain.Coachs;
+import menu.domain.singleton.Restaurant;
 
 /**
  * 코치 ㄱ개체를 생성시켜 반환하고 컨트로럴에서 for문을 돌려 각 몫먹는 음식을 저장한다.
  */
 public class CoachHateMenuService {
+    private final Restaurant restaurant;
+
+    public CoachHateMenuService() {
+        restaurant = Restaurant.RESTAURANT;
+    }
+
     /**
      * 존재하는 메뉴인지 서비스에서 유효검사 못먹는 음식 개수 수*
      *
@@ -27,6 +34,9 @@ public class CoachHateMenuService {
     public void updateHateMenuByCoach(Coach coach, List<String> hateMenus) {
         if (hateMenus.isEmpty()) {
             return;
+        }
+        for (String menuName : hateMenus) {
+            restaurant.validateMenu(menuName);
         }
         coach.addHateMenus(hateMenus);
     }
