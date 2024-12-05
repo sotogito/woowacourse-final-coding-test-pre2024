2ae19f747bd18e703d3a81f4d909d0ba0094d9e2
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-05 15:01:26 +0900

    feat : 코치 이름, 못먹는 음식 DTO 작성

---
+++ src/main/java/menu/dto/CoachDto.java
@@ -0,0 +1,14 @@
+package menu.dto;
+
+public class CoachDto {
+    private String name;
+
+    public CoachDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
---
+++ src/main/java/menu/dto/HateMenuDto.java
@@ -0,0 +1,13 @@
+package menu.dto;
+
+public class HateMenuDto {
+    private String name;
+
+    public HateMenuDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
